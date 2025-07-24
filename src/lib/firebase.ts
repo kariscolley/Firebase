a9253b016924af510eef8cec5c627bcd6885f049
Karis Colley <kariscolley2@gmail.com>
2025-07-24 19:36:22 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  projectId: "ramp-link",
-  appId: "1:1068230064402:web:6352ee474c3cf5a51f1bcb",
-  storageBucket: "ramp-link.firebasestorage.app",
-  apiKey: "AIzaSyCkooZMgcDWg-_KMSDnBmg1oYRtRtERmPs",
-  authDomain: "ramp-link.firebaseapp.com",
-  measurementId: "",
-  messagingSenderId: "1068230064402",
+  "projectId": "ramp-link",
+  "appId": "1:1068230064402:web:6352ee474c3cf5a51f1bcb",
+  "storageBucket": "ramp-link.firebasestorage.app",
+  "apiKey": "AIzaSyCkooZMgcDWg-_KMSDnBmg1oYRtRtERmPs",
+  "authDomain": "ramp-link.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1068230064402"
 };
 
 // Initialize Firebase
